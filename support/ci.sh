63f0a821174e5123ca0ee3db86f239e1dc1897b8
Adam Schwalm <adamschwalm@gmail.com>
2019-06-17 19:03:31 -0500

    Update janet version for ci (to include new parser api)

--- support/ci.sh
+++ support/ci.sh
@@ -2,7 +2,7 @@
 
 set -uex
 
-janetver="95eb54045fe124b83298666d38404f5bff46f515"
+janetver="9c89d1c658a11a40c4a97bd88a9321382c26fda5"
 janeturl="https://github.com/janet-lang/janet/archive/${janetver}.tar.gz"
 mkdir -p ci_builds
 prefix="$(readlink -f ./ci_builds)/installed"
